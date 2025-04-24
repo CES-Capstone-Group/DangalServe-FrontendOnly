adb28b0ce314468674758ef229580bcc2a9b91bd
Laurence Andrew B. Santos <laurencesantos62@gmail.com>
2025-04-24 11:38:06 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://pamantasances.pythonanywhere.com";
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://pamantasances.pythonanywhere.com";
+// const BASE_URL = "http://127.0.0.1:8000";
 const MEDIA_BASE_URL = `${BASE_URL}`;
 
 export const API_ENDPOINTS = {
